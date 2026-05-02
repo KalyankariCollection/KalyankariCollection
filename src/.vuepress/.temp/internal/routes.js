b45a66933af6bd0ef4006f2e39d91b6e22b63955
KalyankariCollection <KalyankariUtilityCredential@gmail.com>
2026-05-02 15:54:00 +0530

    Mystery Commit

--- src/.vuepress/.temp/internal/routes.js
+++ src/.vuepress/.temp/internal/routes.js
@@ -25,20 +25,3 @@ export const routes = Object.fromEntries([
   ["/wiki/Windows.html", { loader: () => import(/* webpackChunkName: "wiki_Windows.html" */"D:/kalyankari-collection/src/.vuepress/.temp/pages/wiki/Windows.html.js"), meta: {"title":"Windows","icon":"mdi:microsoft-windows"} }],
   ["/404.html", { loader: () => import(/* webpackChunkName: "404.html" */"D:/kalyankari-collection/src/.vuepress/.temp/pages/404.html.js"), meta: {"title":""} }],
 ]);
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updateRoutes) {
-    __VUE_HMR_RUNTIME__.updateRoutes(routes)
-  }
-  if (__VUE_HMR_RUNTIME__.updateRedirects) {
-    __VUE_HMR_RUNTIME__.updateRedirects(redirects)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ routes, redirects }) => {
-    __VUE_HMR_RUNTIME__.updateRoutes(routes)
-    __VUE_HMR_RUNTIME__.updateRedirects(redirects)
-  })
-}
